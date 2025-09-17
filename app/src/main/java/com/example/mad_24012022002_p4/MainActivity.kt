e9be540564c6620d3b2d50c671ac2f6e6f299410
OM KUSHWAHA <omkushwaha2005@gmail.com>
2025-09-17 11:17:18 +0530

    Initial commit...

--- app/src/main/java/com/example/mad_24012022002_p4/MainActivity.kt
+++ app/src/main/java/com/example/mad_24012022002_p4/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import java.util.*
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
